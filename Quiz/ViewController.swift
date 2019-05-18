3822fda1eac4148b0324edcad9ac92ea009eaefc
Jen Gilbert <jgilbert@lyft.com>
2019-05-18 13:45:30 -0700

    Set up connections

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
 
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showAnswer(_ sender:UIButton) {
+        
+    }
 
 }
 
